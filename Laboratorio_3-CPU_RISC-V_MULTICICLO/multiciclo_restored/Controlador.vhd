4bbc8204941d75ada54d5d5ad5ffa6b24af64068
Gabriel Castro <gabriel.alcantara.castro@gmail.com>
2025-06-19 22:22:25 -0300

    Corrigi o esboço do código do controlador, mas PROVAVELMENTE tem problema em relação aos bits setados. Acho que o ST_WRITEBACK tá errado, o ChatGPT e o DeepSeek alertaram, mas queria saber a opinião de vocês. Se puderem dar uma olhada também

--- Laboratorio_3-CPU_RISC-V_MULTICICLO/multiciclo_restored/Controlador.vhd
+++ Laboratorio_3-CPU_RISC-V_MULTICICLO/multiciclo_restored/Controlador.vhd
@@ -22,7 +22,7 @@ entity Controlador is
 		Mem2Reg		  : out std_logic_vector(1 downto 0);
 		
 		EscreveMem	  : out std_logic;
-		LeMem 		  : out std_logic;
+		LeMem 		  : out std_logic
 	);
 end Controlador;
 
@@ -30,33 +30,39 @@ architecture bdf_type of controlador is
 
 -- Estados FSM (Máquina de Estados Finitas)
 type state_type is (
-	ST_FETCH; 	  -- Busca a instrução
-	ST_DECODE; 	  -- Decodifica
-	ST_EXECUTE;	  -- Execução (cálculo na ULA)
-	ST_MEMORY; 	  -- Acesso a memória
-	ST_WRITEBACK; -- Escrita no banco de registradores
-);
+	ST_FETCH, 	  
+	ST_DECODE, 	  
+	ST_SW,
+	ST_LW1,
+	ST_LW2,
+	ST_TipoR,
+	ST_BEQ,
+	ST_JAL,
+	ST_JALR,
+	ST_ADDI,
+	ST_WRITEBACK
+	);
 signal pr_state, nx_state: state_type; -- Estado presente e Proximo estado
 
-signal EscreveIR     : out std_logic;
-signal EscrevePCCond : out std_logic;
-signal EscrevePCBack : out std_logic;
-signal EscrevePC	   : out std_logic;
-signal OrigPC		   : out std_logic;
+signal aux_EscreveIR     : std_logic;
+signal aux_EscrevePCCond : std_logic;
+signal aux_EscrevePCBack : std_logic;
+signal aux_EscrevePC	   : std_logic;
+signal aux_OrigPC		   : std_logic;
 
-signal OrigAULA	   : out std_logic_vector(1 downto 0);
-signal OrigBULA 	   : out std_logic_vector(1 downto 0);
-signal ALUOp			: out std_logic_vector(1 downto 0);
+signal aux_OrigAULA	   : std_logic_vector(1 downto 0);
+signal aux_OrigBULA 	   : std_logic_vector(1 downto 0);
+signal aux_ALUOp			: std_logic_vector(1 downto 0);
 
-signal EscreveReg	   : out std_logic;
-signal Mem2Reg		   : out std_logic_vector(1 downto 0);
+signal aux_EscreveReg	   : std_logic;
+signal aux_Mem2Reg		   : std_logic_vector(1 downto 0);
 
-signal EscreveMem	   : out std_logic;
-signal LeMem 		   : out std_logic;
+signal aux_EscreveMem	   : std_logic;
+signal aux_LeMem 		   : std_logic;
 
 begin
 
-	EscreveIR     <= aux_EscreveIR;S
+	EscreveIR     <= aux_EscreveIR;
 	EscrevePCCond <= aux_EscrevePCCond;
 	EscrevePCBack <= aux_EscrevePCBack;			
 	EscrevePC 	  <= aux_EscrevePC;
@@ -74,18 +80,17 @@ begin
 
 process(iCLK, iRST)
 	begin
-	if rising_edge(iCLK) then
-		if (iRST = '1')
-			pr_state <= ST_FETCH;
-		else
-			pr_state <= nx_state;
-	end if;
+		if rising_edge(iCLK) then
+			if (iRST = '1') then
+				pr_state <= ST_FETCH;
+			else
+				pr_state <= nx_state;
+			end if;
+		end if;
 end process;
 
 process(pr_state, opcode)
 	begin
-		nx_state <= ST_FETCH; -- Buscando a próxima instrução
-		
 		aux_EscreveIR		<= '0';
 		aux_EscrevePCCond <= '0';
 		aux_EscrevePCBack <= '0';
@@ -102,16 +107,144 @@ process(pr_state, opcode)
       aux_EscreveMem 	<= '0';
 		aux_LeMem 	   	<= '0';
 		
+		nx_state <= ST_FETCH; -- Buscando a próxima instrução
+		
 		case pr_state is
 			when ST_FETCH 		=>
+				aux_EscreveIR		<= '1';
+				aux_EscrevePC  	<= '1';
+				aux_OrigPC 	   	<= '0';
 				
+				aux_OrigAULA   	<= "10";
+				aux_OrigBULA   	<= "01";
+				aux_ALUOp      	<= "00";
+				
+				nx_state <= ST_DECODE;
 				
 			when ST_DECODE 	=>
-			
-			when ST_EXECUTE	=>
-			
-			when ST_MEMORY 	=>
-			
-			when ST_WRITEBACK =>
+				aux_OrigAULA   	<= "00";
+				aux_OrigBULA   	<= "10";
+				aux_ALUOp      	<= "00";
+				
+				case opcode is 
+					-- sw
+					when "0100011" => 
+						nx_state <= ST_SW;
+					
+					-- lw
+					when "0000011" => 
+						nx_state <= ST_LW1;
+					
+					-- Tipo-R
+					when "0110011" => 
+						nx_state <= ST_TipoR;
+					
+					-- beq
+					when "1100011" => 
+						nx_state <= ST_BEQ;
+					
+					-- jal
+					when "1101111" => 
+						nx_state <= ST_JAL;
+					
+					-- jalr
+					when "1100111" => 
+						nx_state <= ST_JALR;
+					
+					-- addi
+					when "0010011" => 
+						nx_state <= ST_ADDI;
+						
+					when others    => 
+						nx_state <= ST_FETCH;	
+				end case;
+						
+				when ST_SW =>
+					aux_OrigAULA    <= "01";
+					aux_OrigBULA    <= "10";
+					aux_ALUOp       <= "00";
+					
+					aux_EscreveMem  <= '1';
 
-end bdf_type;
+					nx_state <= ST_FETCH;
+					
+				when ST_LW1 =>
+					
+					aux_OrigAULA    <= "01";
+					aux_OrigBULA    <= "10";
+					aux_ALUOp       <= "00";
+			 
+					nx_state <= ST_LW2;
+					
+				when ST_LW2 =>
+					aux_EscreveReg  <= '1';
+					aux_Mem2Reg 	 <= "10";  
+					aux_LeMem       <= '1';
+
+					nx_state <= ST_FETCH;
+					
+				when ST_TipoR =>
+					aux_OrigAULA    <= "01";
+					aux_OrigBULA    <= "00";
+					aux_ALUOp       <= "10";
+					
+					aux_EscreveReg  <= '1';
+					aux_Mem2Reg     <= "00"; 
+					
+					aux_EscreveMem  <= '0';
+
+					nx_state <= ST_WRITEBACK;
+					
+				when ST_BEQ =>
+					aux_EscrevePCCond <= '1';
+					aux_OrigPC      <= '0'; 
+						
+					aux_OrigAULA    <= "01";
+					aux_OrigBULA    <= "00";
+					aux_ALUOp       <= "01";
+						
+					nx_state <= ST_FETCH;
+					
+				when ST_JAL =>
+					aux_EscrevePC   <= '1';
+					aux_OrigPC      <= '1';  
+					aux_EscreveReg  <= '1';
+					aux_Mem2Reg   	 <= "01"; 
+     
+					nx_state <= ST_FETCH;
+					
+				when ST_JALR =>
+					aux_EscrevePC   <= '1';
+					aux_OrigPC      <= '1';  
+					
+					aux_OrigAULA    <= "01";
+					aux_OrigBULA    <= "10";
+					aux_ALUOp       <= "00";
+					
+					aux_EscreveReg  <= '1';
+					aux_Mem2Reg   	 <= "01"; 
+
+					nx_state <= ST_FETCH;
+					
+				when ST_ADDI =>
+					aux_OrigAULA    <= "01";
+					aux_OrigBULA    <= "10";
+					aux_ALUOp       <= "00";
+						
+					aux_EscreveReg  <= '1';
+					aux_Mem2Reg   	 <= "00";
+						
+					aux_EscreveMem  <= '0';
+
+					nx_state <= ST_WRITEBACK;
+						
+				when ST_WRITEBACK =>
+					aux_Mem2Reg   	 <= "00"; 
+					aux_EscreveMem  <= '1';
+
+					nx_state <= ST_FETCH;
+					
+	end case;
+
+end process;
+end architecture;
